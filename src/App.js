9ab1f05144690e0428c93a1f09ead11a0ffcd640
Nnenna Ndukwe <nndukwe@oreilly.com>
2020-04-22 10:43:13 -0400

    changing yarn lock file

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ class App extends Component {
           <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
           <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
           <link href="assets/css/style.css" rel="stylesheet" />
+          <script></script>
                 <div className="container-fluid">
                     <div className="navbar-header">
                     <a className="navbar-brand" href="#">UZERS</a>
